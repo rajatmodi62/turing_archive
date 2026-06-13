3504a6714a524bcc400cea4a568063fcccba63aa
rmodi <rajatmodi62@gmail.com>
2026-06-13 10:51:29 -0400

    a

---
+++ gemini_pipeline/turing_archive/transcribe.sh
@@ -0,0 +1,23 @@
+conda activate turing 
+cd unpublished_drafts
+python process.py
+
+# conda activate turing 
+# cd publications
+# python process.py
+
+# conda activate turing 
+# cd delilah
+# python process.py
+
+# conda activate turing 
+# cd mother_correspondance
+# python process.py
+
+# conda activate turing 
+# cd kings_patricia
+# python process.py
+
+# conda activate turing 
+# cd personal_documents
+# python process.py
